a956c14711bbb297309135ab7766e58a089ca79f
traviscult <60018617+traviscult@users.noreply.github.com>
2023-04-05 17:04:53 -0500

    added works section with card groups and styling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header/Header";
 import IndexNavbar from "./components/Nav/Nav";
 import Skills from "./components/Skills/Skills";
+import Works from "./components/Work/Work";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <IndexNavbar />
       <Header />
       <Skills />
+      <Works />
     </div>
   );
 }
