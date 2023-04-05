f6f9544b6e8f14ac466954c1804c09717a4e15be
traviscult <60018617+traviscult@users.noreply.github.com>
2023-04-05 15:34:38 -0500

    added image and text to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,30 @@
+import "./Header.css";
 import { Button, Container } from "react-bootstrap";
 import Resume from "../../assets/resume/Travis-Cultreri-Resume.pdf";
 
 const Header = () => {
   return (
     <>
-      <Button
-        rel="noopener noreferrer"
-        target="_blank"
-        href={Resume}
-        variant="light"
-      >
-        Resume
-      </Button>
+      <div className="page-header">
+        <Container>
+          <div className="name">
+            <h1>Travis Cultreri</h1>
+            <img
+              className="img-responsive img-travis"
+              src={require("../../assets/img/Travis.jpg")}
+            />
+            <h3>Full Stack Web Devloper.</h3>
+          </div>
+        </Container>
+        <Button
+          rel="noopener noreferrer"
+          target="_blank"
+          href={Resume}
+          variant="light"
+        >
+          Download Resume
+        </Button>
+      </div>
     </>
   );
 };
