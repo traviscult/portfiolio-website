4c14bbff525b2f0adf7441678db24e20dba68d56
traviscult <60018617+traviscult@users.noreply.github.com>
2023-04-05 22:39:16 -0500

    added employee diectory app

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -11,16 +11,24 @@ const Works = () => {
           <Col med="4">
             <Card className="card-avatar">
               <div>
-                <img
-                  class="card-img-top embed-responsive-item"
-                  alt="Reddit Minimal Home Page"
-                  src={require("../../assets/img/reddit-minimal-app.jpg")}
-                />
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://reddit-minimal-react-app.netlify.app/"
+                  // onClick={(e) => e.preventDefault()}
+                >
+                  <img
+                    class="card-img-top embed-responsive-item"
+                    alt="Reddit Minimal Home Page"
+                    src={require("../../assets/img/reddit-minimal-app.jpg")}
+                  />
+                </a>
               </div>
             </Card>
             <Card.Body>
               <a
                 target="_blank"
+                rel="noopener noreferrer"
                 href="https://reddit-minimal-react-app.netlify.app/"
                 // onClick={(e) => e.preventDefault()}
               >
@@ -52,29 +60,34 @@ const Works = () => {
                 <a
                   target="_blank"
                   rel="noopener noreferrer"
-                  href="reddit-minimal-react-app.netlify.app/"
+                  href="https://react-employee-directory-app.netlify.app/"
                 >
                   <img
+                    class="card-img-top embed-responsive-item"
                     alt="Reddit Minimal Home Page"
-                    src={require("../../assets/img/landing-header.jpg")}
+                    src={require("../../assets/img/employee-directory.jpg")}
                   />
                 </a>
               </div>
             </Card>
             <Card.Body>
               <a
-                href="https://reddit-minimal-react-app.netlify.app/"
-                onClick={(e) => e.preventDefault()}
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://react-employee-directory-app.netlify.app/"
+                // onClick={(e) => e.preventDefault()}
               >
                 <div className="author">
-                  <Card.Title tag="h4">Reddit Minimal</Card.Title>
+                  <Card.Title tag="h4">Employee Directory</Card.Title>
                   <h6 className="card-category">Application</h6>
                 </div>
               </a>
               <p className="card-description text-center">
-                This application allows you browse and search Reddit. With the
-                use of React all elements are dynamicly dispalyed and Redux.js
-                manages state.
+                A react application that allows you to access your companies
+                employees information. An employee or manager would benefit
+                greatly from being able to view non-sensitive data about other
+                employees. It would be particularly helpful to be able to filter
+                employees by name.
               </p>
             </Card.Body>
             <Card.Footer className="gitHubBtn">
@@ -82,7 +95,7 @@ const Works = () => {
                 variant="light"
                 target="_blank"
                 rel="noopener noreferrer"
-                href="https://github.com/traviscult/reddit-minimal"
+                href="https://github.com/traviscult/employee-directory"
               >
                 <FaGithub />
               </Button>
